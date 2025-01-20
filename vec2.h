cbbfced3bb632a4c544d14c1ccf173c876fe1666
Kimball Thurston <kdt3rd@gmail.com>
2025-01-20 13:48:08 +1300

    make functions static inline
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- vec2.h
+++ vec2.h
@@ -10,23 +10,23 @@ typedef struct {
 }
 vec2_t;
 
-inline vec2_t vec2_add(vec2_t p1, vec2_t p2) {
+static inline vec2_t vec2_add(vec2_t p1, vec2_t p2) {
   return (vec2_t){.v0=p1.v0 + p2.v0, .v1 = p1.v1 + p2.v1};
 }
 
-inline vec2_t vec2_subtract(vec2_t p1, vec2_t p2) {
+static inline vec2_t vec2_subtract(vec2_t p1, vec2_t p2) {
   return (vec2_t){.v0=p1.v0 - p2.v0, .v1 = p1.v1 - p2.v1};
 }
 
-inline float vec2_dot(vec2_t p1, vec2_t p2) {
+static inline float vec2_dot(vec2_t p1, vec2_t p2) {
   return (p1.v0 * p2.v0) + (p1.v1 * p2.v1);
 }
 
-inline vec2_t vec2_scale(vec2_t p, float scalar) {
+static inline vec2_t vec2_scale(vec2_t p, float scalar) {
   return (vec2_t){.v0 = p.v0 * scalar, .v1 = p.v1 * scalar};
 }
 
-inline float vec2_length(vec2_t vec) {
+static inline float vec2_length(vec2_t vec) {
   return sqrt((vec.v0 * vec.v0) + (vec.v1 * vec.v1));
 }
 
